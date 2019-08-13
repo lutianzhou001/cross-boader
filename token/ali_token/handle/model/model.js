5ef696f03e74db4fd47fe900382fa784f8a3efec
lutianzhou001 <lutianzhou@yuhu.tech>
2019-08-13 18:03:43 +0800

    beautify

--- token/ali_token/handle/model/model.js
+++ token/ali_token/handle/model/model.js
@@ -10,44 +10,38 @@ let connection = mysql.createConnection({
 })
 
 async function saveData(res, contract_name, content) {
-
   let queryTable = 'show full columns from ' + contract_name
-  
-  connection.query(queryTable, function (err, data) { if (err) { console.log(err) } else {
+  connection.query(queryTable, function (err, data) {
+    if (err) { console.log(err) } else {
 
-  var data_columns = ''
-  
-  for (i = 1; i < data.length -1 ; i++ ){
-     data_columns = data_columns +  data[i].Field + ','
-}
-  data_columns = data_columns + data[i].Field
+      var data_columns = ''
 
-  var insert_columns = ''
-  for (i=1; i<data.length -1 ;i++){
-     insert_columns = insert_columns + '?' + ','
-}
-  insert_columns = insert_columns + '?'
+      for (i = 1; i < data.length - 1; i++) {
+        data_columns = data_columns + data[i].Field + ','
+      }
+      data_columns = data_columns + data[i].Field
 
-
-  var obj = []
-  
-  for (i=0 ;i < content.length; i++){
-    for( var key in content[i]){
-      obj.push(content[i][key])}}
-
-
-console.log(obj)
-
-
-  let saveData = 'INSERT INTO '+ contract_name + '(' + data_columns + ') VALUES (' + insert_columns + ')' 
-
-
-  connection.query(saveData, obj, (err,res){ if (err){console.log(err)} else {console.log("success")}})
-  
-} })
-
-  
-
+      var insert_columns = ''
+      for (i = 1; i < data.length - 1; i++) {
+        insert_columns = insert_columns + '?' + ','
+      }
+      insert_columns = insert_columns + '?'
+      var obj = []
+      for (i = 0; i < content.length; i++) {
+        for (var key in content[i]) {
+          obj.push(content[i][key])
+        }
+      }
+      console.log(obj)
+      let saveData = 'INSERT INTO ' + contract_name + '(' + data_columns + ') VALUES (' + insert_columns + ')'
+      connection.query(saveData, obj, (err, res) => {
+        if (err) {
+          console.log(err)
+        }
+        console.log("success")
+      })
+    }
+  })
 }
 
 
