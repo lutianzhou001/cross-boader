13461a196b4d137fec509b2ecd609b0c85508a9c
lutianzhou001 <lutianzhou@yuhu.tech>
2019-08-26 13:40:10 +0800

    fix all functions onChain

--- token/ali_token/contracts/zeppelin-solidity/test/helpers/web3.js
+++
@@ -1,9 +0,0 @@
-const pify = require('./node_modules/pify');
-
-const ethAsync = pify(web3.eth);
-
-module.exports = {
-  ethGetBalance: ethAsync.getBalance,
-  ethSendTransaction: ethAsync.sendTransaction,
-  ethGetBlock: ethAsync.getBlock,
-};
--- truffle/onChainFunction/onChainFunction.js
+++ truffle/onChainFunction/onChainFunction.js
@@ -1,60 +1,73 @@
 var Web3 = require('web3')
 var contract = require("truffle-contract")
-var provider  = new Web3.providers.HttpProvider("http://119.3.43.136:8203")
-var contract  = require("truffle-contract")
+var provider = new Web3.providers.HttpProvider("http://119.3.43.136:8203")
+var contract = require("truffle-contract")
 var fs = require('fs')
 
 
-async function insertOnChain(num,orderId,keys,values){
-
-
-var promiseOnChain = new Promise(function(resolve,reject){
-  
-  fs.readFile('../build/contracts/International.json','utf8',async function(err,data){
+async function insertOnChain(num, orderId, keys, values) {
+  var promiseOnChain = new Promise(function (resolve, reject) {
+    fs.readFile('../build/contracts/International.json', 'utf8', async function (err, data) {
       if (err) console.log(err);
       var International = JSON.parse(data)
       var MyContract = contract(International)
       MyContract.setProvider(provider)
-      MyContract.at("0x56956D9E28356031395FbBC2D30dFaBCe9E1f958").then(function(instance){return instance.insertconfirmPurchase(num,orderId,keys,values,{from:"0x14ca04ff85747def87d6c6c566db84cc24e4643b"})}).then(function(result){resolve(result)})
+      MyContract.at("0x56956D9E28356031395FbBC2D30dFaBCe9E1f958").then(function (instance) {
+        return instance.insertconfirmPurchase(num, orderId, keys, values, {
+          from: "0x14ca04ff85747def87d6c6c566db84cc24e4643b"
+        })
+      }).then(function (result) {
+        resolve(result)
+      })
+    })
   })
-})
-  var Q = await promiseOnChain.then(function(value){return value})
-  console.log(Q.receipt.transactionHash)
+  var res = await promiseOnChain.then(function (value) { return value })
+  console.log(res.receipt.transactionHash)
 }
 
 
-async function queryOnChain(num,orderId){
-fs.readFile('../build/contracts/International.json','utf8',function(err,data){
-  if (err) console.log(err);
-  var International = JSON.parse(data)
-  var MyContract = contract(International)
-  MyContract.setProvider(provider)
-  MyContract.at("0x56956D9E28356031395FbBC2D30dFaBCe9E1f958").then(function(instance){
-    return instance.queryconfirmPurchase.call(0,"orderId002","key002",["value001","value002"])
-}).then(function(result){console.log(result)})
-})
+async function queryOnChain(num, orderId) {
+  var promiseQueryChain = new Promise(function (resolve, reject) {
+    fs.readFile('../build/contracts/International.json', 'utf8', function (err, data) {
+      if (err) console.log(err);
+      var International = JSON.parse(data)
+      var MyContract = contract(International)
+      MyContract.setProvider(provider)
+      MyContract.at("0x56956D9E28356031395FbBC2D30dFaBCe9E1f958").then(function (instance) {
+        return instance.queryconfirmPurchase.call(num, orderId, "key002", ["value001", "value002"])
+      }).then(function (result) {
+        resolve(result)
+      })
+    })
+  })
+  var res = await promiseQueryChain.then(function (value) { return value })
+  console.log(res)
 }
 
 
 
-async function queryTotalonChain(){
-fs.readFile('../build/contracts/International.json','utf8',function(err,data){
-  if (err) console.log(err);
-  var International = JSON.parse(data)
-  var MyContract = contract(International)
-  MyContract.setProvider(provider)
-  MyContract.at("0x56956D9E28356031395FbBC2D30dFaBCe9E1f958").then(function(instance){
-    return instance.queryTotalPurchase.call()
-}).then(function(result){console.log(result)})
-})
+async function queryTotalonChain() {
+  var promiseQueryTotalChain = new Promise(function (resolve, reject) {
+    fs.readFile('../build/contracts/International.json', 'utf8', function (err, data) {
+      if (err) console.log(err);
+      var International = JSON.parse(data)
+      var MyContract = contract(International)
+      MyContract.setProvider(provider)
+      MyContract.at("0x56956D9E28356031395FbBC2D30dFaBCe9E1f958").then(function (instance) {
+        return instance.queryTotalPurchase.call()
+      }).then(function (result) {
+        console.log(result)
+      })
+    })
+  })
+  var res = await promiseQueryTotalChain.then(function (value) { return, value })
+  console.log(res)
 }
-
-insertOnChain(0,"sdas",["sada","sdasa"],["vasva","vava"])
 
 module.exports = {
 
-insertOnChain,
-queryOnChain,
-queryTotalonChain
+  insertOnChain,
+  queryOnChain,
+  queryTotalonChain
 
 }
