f50a768254405e8c4906977a131c421b17eb4886
lutianzhou001 <lutianzhou@yuhu.tech>
2019-08-29 14:42:14 +0800

    modified the contract

--- truffle/contracts/CRUD.sol
+++ truffle/contracts/CRUD.sol
@@ -9,7 +9,8 @@ contract International {
     IterableMapping.itmap public assetdata;
     
     struct confirmPurchase {
-        string OrderId;    //Confirmpurchase
+        string orderId;    //Confirmpurchase
+        string contract_name;
         //string createAt;    //1566278738 uinx
         IterableMapping.itmap confirmPurchaseData;    //map{"buyer","tdergouzi"}
     }
@@ -19,30 +20,12 @@ contract International {
         totalConfirmPurchases = 0;
     }
     
-    struct shipping {
-        string OrderId;    //Transportation
-        //string createAt;    //1566278924 uinx
-        IterableMapping.itmap shippingData; //
-    }
-    
-    struct label {
-        string OrderId;    //Marking
-        //string createAt;    //1566278924 uinx
-        IterableMapping.itmap labelData; 
-    }
-    
-    struct store {
-        string OrderId; //Into
-        //string createAt;
-        IterableMapping.itmap storeData;
-    }
-    
-    
     mapping(uint256 => confirmPurchase)  tokenIdtoconfirmPurchase;
     
-    function insertconfirmPurchase(uint256 _num,string memory OrderId,string[] memory keys, string[] memory values) public returns(bool){
+    function insertconfirmPurchase(uint256 _num,string memory orderId,string memory contract_name,string[] memory keys, string[] memory values) public returns(bool){
         confirmPurchase storage newConfirmPurchase = tokenIdtoconfirmPurchase[_num];
-        newConfirmPurchase.OrderId = OrderId;
+        newConfirmPurchase.orderId = orderId;
+        newConfirmPurchase.contract_name = contract_name;
         for (uint i = 0; i < keys.length; i++) {
             IterableMapping.insert(newConfirmPurchase.confirmPurchaseData,keys[i], values[i]);
         }
@@ -50,22 +33,41 @@ contract International {
         return true;
     }
     
-    function queryconfirmPurchase(uint256 _num, string memory OrderId,string memory key, string[] memory values) public returns(string memory){
-         for (uint i = 1; i < totalConfirmPurchases + 1 ; i++){
-             if (StringTools.compareStrings(tokenIdtoconfirmPurchase[i].OrderId, OrderId)){
-                string memory KVS = keyValues(tokenIdtoconfirmPurchase[i].confirmPurchaseData);         
-                //string memory values = values(tokenIdtoconfirmPurchase[i].confirmPurchaseData);
-                //string memory results = StringTools._strConcat(keys,values);
-                return KVS;
+    function queryconfirmPurchase(uint256 _num, string memory orderId, string memory contract_name, string memory key, string[] memory values) public returns(string memory){
+        string memory results;
+        for (uint i=1; i<totalConfirmPurchases+1;i++){
+             //searching contract_name,ignore which orderId is 
+             if (  (StringTools.compareStrings2(orderId, "NA"))    &&   (!(StringTools.compareStrings2(contract_name, "NA"))) ){
+               if (StringTools.compareStrings(tokenIdtoconfirmPurchase[i].contract_name, contract_name)){
+                string memory KVS = keyValues(tokenIdtoconfirmPurchase[i].confirmPurchaseData);
+                results = StringTools._strConcat(results,KVS);
              }
-         }
-         //return tokenIdtoconfirmPurchase[_num].confirmPurchaseData.data[key].value;
+           }
+              //searching orderId, ignore which contract_name is 
+              if (  (StringTools.compareStrings2(contract_name, "NA"))    &&   (!(StringTools.compareStrings2(orderId, "NA"))) ){
+                if (StringTools.compareStrings(tokenIdtoconfirmPurchase[i].orderId, orderId)){
+                string memory KVS = keyValues(tokenIdtoconfirmPurchase[i].confirmPurchaseData);
+                results = StringTools._strConcat(results,KVS);
+             }
+           }
+               //searching both orderId and contract_name
+              if (  (!(StringTools.compareStrings2(contract_name, "NA")))    &&   (!(StringTools.compareStrings2(orderId, "NA"))) ){
+                if (   (StringTools.compareStrings(tokenIdtoconfirmPurchase[i].orderId, orderId))   &&     (StringTools.compareStrings(tokenIdtoconfirmPurchase[i].contract_name, contract_name)) ){
+                string memory KVS = keyValues(tokenIdtoconfirmPurchase[i].confirmPurchaseData);
+                results = StringTools._strConcat(results,KVS);
+             }
+           }
+        }
+        return results;
     }
+    
+    //function querybyContractname(...){
+
     
     function queryTotalPurchase() public returns(uint256){
         return totalConfirmPurchases;
     }
-
+    
     function keyValues(IterableMapping.itmap storage im) internal returns (string memory) {
         string memory keys;
         string memory values;
@@ -82,10 +84,8 @@ contract International {
                 values = StringTools._strConcat(values,value);
             }
         }
-        //return values;
         return results = StringTools._strConcat(keys,values);
-        //return results;
+        
     }
-    
 }
     
