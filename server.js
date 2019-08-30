773959cb178bcd1d2667737b6d319b61090d0577
lutianzhou001 <lutianzhou@yuhu.tech>
2019-08-30 16:33:01 +0800

    add three functions to fetch data on chain

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const utils = require('./token/ali_token/utils/utils')
 const model = require('./token/ali_token/handle/model/model')
 const contracts = require('./token/ali_token/handle/modify/contracts')
+const chain = require('./token/ali_token/handle/chain/chain')
 const http = require("http");
 const express = require("express");
 const app = express();
@@ -9,42 +9,49 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/api/contracts/createContract', async function(req, res){
+app.post('/api/contracts/createContract', async function (req, res) {
     console.log("createContract")
     var contract_name = req.body.contract_name
     var content = JSON.stringify(req.body.content)
     var created = Date.now().toString()
-    var result = await contracts.createContract(res,contract_name,content,created)
-    var createTable = await contracts.createTable(res,req.body.contract_name,req.body.content)
-});
+    var result = await contracts.createContract(res, contract_name, content, created)
+    var createTable = await contracts.createTable(res, req.body.contract_name, req.body.content)
+})
 
-app.get('/api/contracts/queryContracts',async function(req,res){
+app.get('/api/contracts/queryContracts', async function (req, res) {
     console.log("queryContracts")
     var result = await contracts.queryContracts(res)
 })
 
-/*
-app.post('/api/contracts/deployContract', async function(req, res){
-    console.log("deployContract")
-    var id = req.body.id
-    var result = await deploy.deployContract(res,id)
-});
-*/
+app.get('/api/chain/queryBlock', async function (req, res) {
+    console.log("queryBlock")
+    var result = await chain.queryBlock(req.body.blockNumber, res)
+})
 
-app.post('/api/models/saveData',async function(req,res){
+app.get('/api/chain/queryTransaction', async function (req, res) {
+    console.log("queryTransaction")
+    var result = await chain.queryTransaction(req.body.txHash, res)
+})
+
+app.get('/api/chain/queryBalance', async function (req, res) {
+    console.log("queryBalance")
+    var result = await chain.queryBalance(req.body.account, res)
+})
+
+app.post('/api/models/saveData', async function (req, res) {
     console.log("saveData")
-    var result = await model.batchSaveData(res,req.body.contract_name,req.body.content) 
+    var result = await model.batchSaveData(res, req.body.contract_name, req.body.content)
 });
 
 
-app.get('/api/models/queryData',async function(req,res){
+app.get('/api/models/queryData', async function (req, res) {
     console.log("queryData")
-    var result = await model.queryData(res,req.body.contract_name,req.body.filter)
+    var result = await model.queryData(res, req.body.contract_name, req.body.filter)
 });
 
 
-http.createServer(app).listen(3000, function(){
+http.createServer(app).listen(3000, function () {
     console.log("server start");
 });
---
+++ token/ali_token/handle/chain/chain.js
@@ -0,0 +1,44 @@
+const Web3 = require('web3')
+
+var web3 = new Web3(new Web3.providers.HttpProvider("http://119.3.43.136:8203"));
+
+async function queryBlock(blockNumber, response) {
+    await web3.eth.getBlock(blockNumber, function (err, res) {
+        if (err) {
+            response.status(200).json(err).end()
+        } else {
+            response.status(200).json(res).end()
+        }
+    })
+}
+
+async function queryBalance(account, response) {
+    await web3.eth.getBalance(account, function (err, res) {
+        if (err) {
+            response.status(200).json(err).end()
+        } else {
+            response.status(200).json(res).end()
+        }
+    })
+}
+
+
+async function queryTransaction(txHash, response) {
+    await web3.eth.getTransaction(txHash, function (err, res) {
+        if (err) {
+            response.status(200).json(err).end()
+        } else {
+            response.status(200).json(res).end()
+        }
+    })
+}
+
+module.exports = {
+    queryBlock,
+    queryBalance,
+    queryTransaction
+}
+
+
+
+
